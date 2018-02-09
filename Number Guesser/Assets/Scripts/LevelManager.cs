a1040f5772ee2f79a81ce922971ce300206a200d
hakbar0 <haseeb2000@hotmail.com>
2018-02-09 17:51:23 +0000

    Able to navigate between scense, whilst clicking the button.

--- Number Guesser/Assets/Scripts/LevelManager.cs
+++ Number Guesser/Assets/Scripts/LevelManager.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
-		Debug.Log ("Level Load requested.");
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void StartLevel(string name) {
+		SceneManager.LoadScene ("Main");
 	}
 
 }
