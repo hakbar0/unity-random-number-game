96fe2208b807bb60454393d2d311b9ce48385f29
hakbar0 <haseeb2000@hotmail.com>
2018-02-09 18:05:31 +0000

    Able to navigate fully between scenes.

--- Number Guesser/Assets/Scripts/LevelManager.cs
+++ Number Guesser/Assets/Scripts/LevelManager.cs
@@ -6,11 +6,7 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
-		SceneManager.LoadScene ("Game");
+		SceneManager.LoadScene (name);
 	}
-
-	public void StartLevel(string name) {
-		SceneManager.LoadScene ("Main");
-	}
-
+		
 }
