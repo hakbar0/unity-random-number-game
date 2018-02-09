9f94b3a57f7d8a4b0c9a45771a9941731f3f01cf
hakbar0 <haseeb2000@hotmail.com>
2018-02-09 18:31:56 +0000

    Fully functional game. Randomly guesses your score.

---
+++ Number Guesser/Assets/Scripts/NumberGuesser.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberGuesser : MonoBehaviour
+{
+	int max, min, guess, maxGuessesAllowed = 8;
+
+	public Text guessText;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartGame ();
+
+	}
+		
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower(){
+		max = guess;
+		NextGuess ();
+	}
+
+
+	void StartGame ()
+	{
+		max = 1000; 
+		min = 1; 
+		NextGuess ();
+	}
+
+	void NextGuess ()
+	{
+		guess = Random.Range (min, max+1);
+		guessText.text = guess.ToString();
+		maxGuessesAllowed -= 1;
+		if (maxGuessesAllowed <= 0)
+			SceneManager.LoadScene ("Win");
+	}
+
+}
+
