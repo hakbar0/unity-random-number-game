e71020a0aef32d4fea20d3cef0afa069a5d06fb9
hakbar0 <haseeb2000@hotmail.com>
2018-02-09 16:53:26 +0000

    Refactored code.

--- Number Guesser/Assets/Scripts/NumberGuessers.cs
+++ Number Guesser/Assets/Scripts/NumberGuessers.cs
@@ -9,18 +9,10 @@ public class NumberGuessers : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		max += 1;
-		print ("Welcome to the number guesser.");
-		print ("Pick a number in your head, don't tell me."); 
-	
-		print ("The highest number you can pick is " + max + ".");
-		print ("The lowest number you can pick is " + min + ".");
-
-		print ("Is the number higher or lower than " + guess + "?");
-		print ("Press up for higher, down for lower and space for equals.");
+		StartGame ();
 
 	}
-	
+		
 	// Update is called once per frame
 	void Update ()
 	{
@@ -28,17 +20,35 @@ public class NumberGuessers : MonoBehaviour
 		if (Input.GetKeyDown ("up")) {
 			print ("up was pressed");
 			min = guess;
-			guess = (max + min) / 2;
-			print ("Is the the number higher or lower than, " + guess + "?");
+			NextGuess ();
 
 		} else if (Input.GetKeyDown ("down")) {
 			print ("down was pressed");
 			max = guess;
-			guess = (max + min) / 2;
-			print ("Is the the number higher or lower than, " + guess + "?");
+			NextGuess ();
 
 		} else if (Input.GetKeyDown ("space"))
 			print ("I won.");
 
 	}
+
+	void StartGame(){
+		max += 1;
+		print ("Welcome to the number guesser.");
+		print ("Pick a number in your head, don't tell me."); 
+
+		print ("The highest number you can pick is " + max + ".");
+		print ("The lowest number you can pick is " + min + ".");
+
+		print ("Is the number higher or lower than " + guess + "?");
+		print ("Press up for higher, down for lower and space for equals.");
+	}
+
+	void NextGuess(){
+		guess = (max + min)/2;
+		print ("Is the the number higher or lower than, " + guess + "?");
+		print ("Press up for higher, down for lower and space for equals.");
+	}
+
 }
+
