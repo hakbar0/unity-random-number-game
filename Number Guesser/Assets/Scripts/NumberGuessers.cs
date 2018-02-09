37f6de5e12f296488bd7f155cbc276f18bc65a07
hakbar0 <haseeb2000@hotmail.com>
2018-02-09 16:17:55 +0000

    Able to display default max and min score.

--- Number Guesser/Assets/Scripts/NumberGuessers.cs
+++ Number Guesser/Assets/Scripts/NumberGuessers.cs
@@ -6,8 +6,14 @@ public class NumberGuessers : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print ("welcome to the number guesser");
-		print ("pick a number in your head, don't tell me"); 
+		print ("Welcome to the number guesser.");
+		print ("Pick a number in your head, don't tell me."); 
+
+		int max = 500;
+		int min = 1;
+
+		print("The highest number you can pick is " + max + ".");
+		print("The lowest number you can pick is " + min + ".");
 	}
 	
 	// Update is called once per frame
