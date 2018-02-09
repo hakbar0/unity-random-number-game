47f4bb9dc93a739e12e7ee81b54c96b7e56ecfa5
hakbar0 <haseeb2000@hotmail.com>
2018-02-09 16:49:05 +0000

    Able to adjust the users guess.

--- Number Guesser/Assets/Scripts/NumberGuessers.cs
+++ Number Guesser/Assets/Scripts/NumberGuessers.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class NumberGuessers : MonoBehaviour
 {
+	int max = 1000, min = 1, guess = 500;
 
 	// Use this for initialization
 	void Start ()
 	{
+		max += 1;
 		print ("Welcome to the number guesser.");
 		print ("Pick a number in your head, don't tell me."); 
-
-		int max = 500;
-		int min = 1;
-
+	
 		print ("The highest number you can pick is " + max + ".");
 		print ("The lowest number you can pick is " + min + ".");
 
-		print ("Is the number higher or lower than 500?");
+		print ("Is the number higher or lower than " + guess + "?");
 		print ("Press up for higher, down for lower and space for equals.");
 
 	}
@@ -25,15 +24,21 @@ public class NumberGuessers : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-			
-		if (Input.GetKeyDown ("space"))
-			print ("space key was pressed.");
-
-		if (Input.GetKeyDown ("up"))
+		
+		if (Input.GetKeyDown ("up")) {
 			print ("up was pressed");
+			min = guess;
+			guess = (max + min) / 2;
+			print ("Is the the number higher or lower than, " + guess + "?");
 
-		if (Input.GetKeyDown ("down"))
+		} else if (Input.GetKeyDown ("down")) {
 			print ("down was pressed");
+			max = guess;
+			guess = (max + min) / 2;
+			print ("Is the the number higher or lower than, " + guess + "?");
+
+		} else if (Input.GetKeyDown ("space"))
+			print ("I won.");
 
 	}
 }
