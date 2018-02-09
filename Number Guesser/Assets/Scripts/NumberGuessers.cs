dba984faec6fdd4dc478ba5c745db7508310074c
hakbar0 <haseeb2000@hotmail.com>
2018-02-09 14:56:29 +0000

    Made a new gameobject and attached a script. Able to print text to the console.

---
+++ Number Guesser/Assets/Scripts/NumberGuessers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberGuessers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("welcome to the number guesser");
+		print ("pick a number in your head, don't tell me"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
